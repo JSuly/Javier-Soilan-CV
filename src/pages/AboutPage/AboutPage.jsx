ab26bfe00e8e7d8f9fd55b6be75a64b3033c31d2
javier-soilan <javier.soilan@gmail.com>
2021-05-19 18:22:34 +0200

    Text corrections

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -15,16 +15,16 @@ export function AboutPage() {
                     </div>
                     
                     <div className="c-about__sulo">
-                        <p>Me llamo Javier, tengo 24 años, soy de A Coruña y soy Desarrollador Full-Stack Jr. Tras varios años trabajando como cocinero, en el año 2020, decidí hacer un cambio en mi vida y en mi carrera profesional, formandome como desarrollador. Tras este tiempo de formación estoy buscando perfeccionar mis habilidades y ampliar mis conocimientos, estudiando, realizando proyectyos propios y buscando empleo.</p>
+                        <p>Me llamo Javier, tengo 24 años, soy de A Coruña y soy Desarrollador Full-Stack Jr. Tras varios años trabajando como cocinero, en el año 2020, decidí hacer un cambio en mi vida y en mi carrera profesional, formándome como desarrollador. Tras este tiempo de formación estoy buscando perfeccionar mis habilidades y ampliar mis conocimientos, estudiando, realizando proyectos propios y buscando empleo.</p>
                     </div>
 
                 </article>
 
                 <article className="c-about__info">
 
-                    <p className="c-about__hobbies">Soy un apasionado de los videojuegos, de la musica y de el deporte. Desde bien pequeño he estado en contancto con ordenadores, consolas y todo tipo de tecnología.
+                    <p className="c-about__hobbies">Soy un apasionado de los videojuegos, de la música y del deporte. Desde bien pequeño he estado en contacto con ordenadores, consolas y todo tipo de tecnología. 
                     <br/>
-                    Durante 10 años, desde los 12-13, he practicado skate, esto es algo que me a marcado como persona. Me ha enseñado a no rendirme, a esforzarme y a seguir superandome cada día.</p>
+                    Durante 10 años, desde los 12-13, he practicado skate, esto es algo que me ha marcado como persona. Me ha enseñado a no rendirme, a esforzarme y a seguir superándome cada día.</p>
 
                 </article>
 
